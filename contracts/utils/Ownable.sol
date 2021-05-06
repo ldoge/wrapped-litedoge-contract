9e2e75071751622c98486af13c3b4348af865387
Huiren Woo <giantcrabby@gmail.com>
2021-05-06 17:27:45 +0800

    Change ownership renounce to pure

--- contracts/utils/Ownable.sol
+++ contracts/utils/Ownable.sol
@@ -45,13 +45,9 @@ contract Ownable is Context {
     }
 
     /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
+     * No renouncing ownership
      */
-    function renounceOwnership() public onlyOwner {
+    function renounceOwnership() public pure {
         revert("renouncing ownership is blocked");
     }
 
