73e30713f58fba52d269b78f0e88d45603fe3da5
Huiren Woo <giantcrabby@gmail.com>
2021-05-13 11:50:00 +0800

    Remove default mint and burn methods

--- test/test-wldoge.js
+++ test/test-wldoge.js
@@ -19,77 +19,65 @@ contract("WLDOGE", accounts => {
         assert.equal(balance.valueOf(), 0);
     });
 
-    it("should mint Wrapped LiteDoge in the first account", async () => {
-        const initialSupply = await wldoge.totalSupply.call();
-        const mintAmount = new BN(1000000);
-        const canMint = await wldoge.mint.call(mintAmount);
-        assert.equal(canMint.valueOf(), true);
-
-        // Carry out actual mint event
-        await wldoge.mint(mintAmount);
-
-        // Check if total supply increase
-        const newSupply = await wldoge.totalSupply.call();
-        assert.equal(newSupply.toNumber(), initialSupply.toNumber() + mintAmount.toNumber());
-
-        // Check if first account HODLer has received it
-        const balance = await wldoge.balanceOf.call(accounts[0]);
-        assert.equal(balance.toNumber(), mintAmount.toNumber());
-    });
-
-    it("should not mint coin using non-owner", async () => {
-        const mintAmount = new BN(1000000);
+    it("should bridge wrap coin to owner account using owner", async () => {
         const ownerAddress = accounts[0];
-        const otherAddress = accounts[1];
-
-        // Get initial balances of owner address
-        let balance = await wldoge.balanceOf.call(ownerAddress);
-        const account_one_starting_balance = balance.toNumber();
-
-        (await wldoge.mint(mintAmount, {from: otherAddress})
-            .catch((err) => {
-                expect(err).to.have.property('message', NOT_OWNER_ERROR_MSG);
-            }));
-
-        balance = await wldoge.balanceOf.call(ownerAddress);
-        const account_one_ending_balance = balance.toNumber();
-
-        assert.equal(
-            account_one_ending_balance,
-            account_one_starting_balance,
-            "Amount was minted using non-owner address"
-        );
-    });
-
-    it("should mint coin directly to other account", async () => {
-        const otherAddress = accounts[1];
 
         const initialSupply = await wldoge.totalSupply.call();
-        const mintAmount = new BN(1000000);
-        const canMint = await wldoge.mintToAddress.call(otherAddress, mintAmount);
-        assert.equal(canMint.valueOf(), true);
+        const bridgeWrapAmount = new BN(10000000000);
+        const liteDogeAddress = 'dcdGuzwAjRH8DwpvyzKyLPdvuArfzXcxtN';
+        const liteDogeTxID = '4f7dcc2de1b7051d42cbbe7a74f11e966457b9fa9effb949759a67bf1c20950f';
+        const canBridgeWrap = await wldoge.bridgeWrap.call(liteDogeAddress, ownerAddress, bridgeWrapAmount, liteDogeTxID);
+        assert.equal(canBridgeWrap.valueOf(), true);
 
-        // Carry out actual mint event
-        await wldoge.mintToAddress(otherAddress, mintAmount);
+        const initialBalance = await wldoge.balanceOf.call(ownerAddress);
+
+        // Carry out actual bridgeWrap event
+        await wldoge.bridgeWrap(liteDogeAddress, ownerAddress, bridgeWrapAmount, liteDogeTxID);
 
         // Check if total supply increase
         const newSupply = await wldoge.totalSupply.call();
-        assert.equal(newSupply.toNumber(), initialSupply.toNumber() + mintAmount.toNumber());
+        assert.equal(newSupply.toNumber(), initialSupply.toNumber() + bridgeWrapAmount.toNumber());
+
+        // Check if HODLer has received it
+        const balance = await wldoge.balanceOf.call(ownerAddress);
+        assert.equal(balance.toNumber(), initialBalance.toNumber() + bridgeWrapAmount.toNumber());
+    });
+
+    it("should bridge wrap coin directly to other account using owner", async () => {
+        const otherAddress = accounts[1];
+
+        const initialSupply = await wldoge.totalSupply.call();
+        const bridgeWrapAmount = new BN(10000000000);
+        const liteDogeAddress = 'dcdGuzwAjRH8DwpvyzKyLPdvuArfzXcxtN';
+        const liteDogeTxID = '4f7dcc2de1b7051d42cbbe7a74f11e966457b9fa9effb949759a67bf1c20950f';
+        const canBridgeWrap = await wldoge.bridgeWrap.call(liteDogeAddress, otherAddress, bridgeWrapAmount, liteDogeTxID);
+        assert.equal(canBridgeWrap.valueOf(), true);
+
+        const initialBalance = await wldoge.balanceOf.call(otherAddress);
+
+        // Carry out actual bridgeWrap event
+        await wldoge.bridgeWrap(liteDogeAddress, otherAddress, bridgeWrapAmount, liteDogeTxID);
+
+        // Check if total supply increase
+        const newSupply = await wldoge.totalSupply.call();
+        assert.equal(newSupply.toNumber(), initialSupply.toNumber() + bridgeWrapAmount.toNumber());
 
         // Check if HODLer has received it
         const balance = await wldoge.balanceOf.call(otherAddress);
-        assert.equal(balance.toNumber(), mintAmount.toNumber());
+        assert.equal(balance.toNumber(), initialBalance.toNumber() + bridgeWrapAmount.toNumber());
     });
 
-    it("should not mint coin directly to other account using non-owner", async () => {
-        const mintAmount = new BN(1000000);
+    it("should not bridge wrap directly to other account using non-owner", async () => {
+        const bridgeWrapAmount = new BN(10000000000);
+        const liteDogeAddress = 'dcdGuzwAjRH8DwpvyzKyLPdvuArfzXcxtN';
+        const liteDogeTxID = '4f7dcc2de1b7051d42cbbe7a74f11e966457b9fa9effb949759a67bf1c20950f';
         const otherAddress = accounts[1];
 
         // Get initial balances of other address
         let balance = await wldoge.balanceOf.call(otherAddress);
         const account_one_starting_balance = balance.toNumber();
 
-        (await wldoge.mintToAddress(otherAddress, mintAmount, {from: otherAddress})
+        (await wldoge.bridgeWrap(liteDogeAddress, otherAddress, bridgeWrapAmount, liteDogeTxID, {from: otherAddress})
             .catch((err) => {
                 expect(err).to.have.property('message', NOT_OWNER_ERROR_MSG);
             }));
@@ -100,7 +88,7 @@ contract("WLDOGE", accounts => {
         assert.equal(
             account_one_ending_balance,
             account_one_starting_balance,
-            "Amount was minted using non-owner address"
+            "Amount was bridge wrapped using non-owner address"
         );
     });
 
@@ -110,9 +98,6 @@ contract("WLDOGE", accounts => {
         const account_two = accounts[1];
 
         const amount = 100;
-
-        // give first account the money first
-        await wldoge.mintToAddress(account_one, amount);
 
         let balance = await wldoge.balanceOf.call(account_one);
         const account_one_starting_balance = balance.toNumber();
@@ -139,135 +124,10 @@ contract("WLDOGE", accounts => {
         );
     });
 
-    it("should burn coin correctly using owner to burn self", async () => {
-        const burnAmt = 100;
-        const ownerAddress = accounts[0];
-
-        // give owner the money first
-        await wldoge.mintToAddress(ownerAddress, burnAmt);
-
-        // Get initial balances of owner account.
-        let balance = await wldoge.balanceOf.call(ownerAddress);
-        const account_one_starting_balance = balance.toNumber();
-
-        await wldoge.burn(ownerAddress, burnAmt);
-
-        balance = await wldoge.balanceOf.call(ownerAddress);
-        const account_one_ending_balance = balance.toNumber();
-
-        assert.equal(
-            account_one_ending_balance,
-            account_one_starting_balance - burnAmt,
-            "Amount wasn't correctly burnt from the owner"
-        );
-    });
-
-    it("should burn coin correctly using owner to burn others", async () => {
-        const burnAmt = 100;
-        const addressToBurn = accounts[1];
-
-        // give address the money first
-        await wldoge.mintToAddress(addressToBurn, burnAmt);
-
-        // Get initial balances of addressToBurn account.
-        let balance = await wldoge.balanceOf.call(addressToBurn);
-        const account_one_starting_balance = balance.toNumber();
-
-        await wldoge.burn(addressToBurn, burnAmt);
-
-        balance = await wldoge.balanceOf.call(addressToBurn);
-        const account_one_ending_balance = balance.toNumber();
-
-        assert.equal(
-            account_one_ending_balance,
-            account_one_starting_balance - burnAmt,
-            "Amount wasn't correctly burnt from the target address"
-        );
-    });
-
-    it("should not burn coin using non-owner to burn others", async () => {
-        const burnAmt = 100;
-        const ownerAddress = accounts[0];
-        const otherAddress = accounts[1];
-
-        // give owner the money first
-        await wldoge.mintToAddress(ownerAddress, burnAmt);
-
-        // Get initial balances of addressToBurn account.
-        let balance = await wldoge.balanceOf.call(ownerAddress);
-        const account_one_starting_balance = balance.toNumber();
-
-        (await wldoge.burn(ownerAddress, burnAmt, {from: otherAddress})
-            .catch((err) => {
-                expect(err).to.have.property('message', NOT_OWNER_ERROR_MSG);
-            }));
-
-        balance = await wldoge.balanceOf.call(ownerAddress);
-        const account_one_ending_balance = balance.toNumber();
-
-        assert.equal(
-            account_one_ending_balance,
-            account_one_starting_balance,
-            "Amount was burnt from the target address using non-owner address"
-        );
-    });
-
-    it("should bridge wrap coin directly to other account", async () => {
-        const otherAddress = accounts[1];
-
-        const initialSupply = await wldoge.totalSupply.call();
-        const bridgeWrapAmount = new BN(1000000);
-        const liteDogeAddress = 'dcdGuzwAjRH8DwpvyzKyLPdvuArfzXcxtN';
-        const liteDogeTxID = '4f7dcc2de1b7051d42cbbe7a74f11e966457b9fa9effb949759a67bf1c20950f';
-        const canBridgeWrap = await wldoge.bridgeWrap.call(liteDogeAddress, otherAddress, bridgeWrapAmount, liteDogeTxID);
-        assert.equal(canBridgeWrap.valueOf(), true);
-
-        const initialBalance = await wldoge.balanceOf.call(otherAddress);
-
-        // Carry out actual bridgeWrap event
-        await wldoge.bridgeWrap(liteDogeAddress, otherAddress, bridgeWrapAmount, liteDogeTxID);
-
-        // Check if total supply increase
-        const newSupply = await wldoge.totalSupply.call();
-        assert.equal(newSupply.toNumber(), initialSupply.toNumber() + bridgeWrapAmount.toNumber());
-
-        // Check if HODLer has received it
-        const balance = await wldoge.balanceOf.call(otherAddress);
-        assert.equal(balance.toNumber(), initialBalance.toNumber() + bridgeWrapAmount.toNumber());
-    });
-
-    it("should not bridge wrap directly to other account using non-owner", async () => {
-        const bridgeWrapAmount = new BN(1000000);
-        const liteDogeAddress = 'dcdGuzwAjRH8DwpvyzKyLPdvuArfzXcxtN';
-        const liteDogeTxID = '4f7dcc2de1b7051d42cbbe7a74f11e966457b9fa9effb949759a67bf1c20950f';
-        const otherAddress = accounts[1];
-
-        // Get initial balances of other address
-        let balance = await wldoge.balanceOf.call(otherAddress);
-        const account_one_starting_balance = balance.toNumber();
-
-        (await wldoge.bridgeWrap(liteDogeAddress, otherAddress, bridgeWrapAmount, liteDogeTxID, {from: otherAddress})
-            .catch((err) => {
-                expect(err).to.have.property('message', NOT_OWNER_ERROR_MSG);
-            }));
-
-        balance = await wldoge.balanceOf.call(otherAddress);
-        const account_one_ending_balance = balance.toNumber();
-
-        assert.equal(
-            account_one_ending_balance,
-            account_one_starting_balance,
-            "Amount was bridge wrapped using non-owner address"
-        );
-    });
-
     it("should bridge unwrap coin correctly using owner", async () => {
         const bridgeUnwrapAmount = new BN(1000000000);
         const liteDogeAddress = 'dcdGuzwAjRH8DwpvyzKyLPdvuArfzXcxtN';
         const ownerAddress = accounts[0];
-
-        // give owner the money first
-        await wldoge.mintToAddress(ownerAddress, bridgeUnwrapAmount);
 
         // Get initial balances of owner account.
         let balance = await wldoge.balanceOf.call(ownerAddress);
@@ -289,9 +149,6 @@ contract("WLDOGE", accounts => {
         const bridgeUnwrapAmount = new BN(1000000000);
         const liteDogeAddress = 'dcdGuzwAjRH8DwpvyzKyLPdvuArfzXcxtN';
         const otherAddress = accounts[1];
-
-        // give sender the money first
-        await wldoge.mintToAddress(otherAddress, bridgeUnwrapAmount);
 
         // Get initial balances of other account.
         let balance = await wldoge.balanceOf.call(otherAddress);
